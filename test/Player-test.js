9073eac1f5904cc86ce91c7eac15ef036d7ddcec
Bruce Chung <brucekchung@gmail.com>
2017-12-02 22:57:17 -0700

    Add backdrop and third canvas

---
+++ test/Player-test.js
@@ -0,0 +1,11 @@
+// const Chai = require('./chai');
+// const assert = chai.assert;
+
+// const Player = require('./Player.js');
+// const player = new Player();
+
+// describe('Player', () => {
+//   it('should not be dead at the start of game', () => {
+//     assert.equal(player.dead, false);
+//   })
+// });
