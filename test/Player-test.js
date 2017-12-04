e44c62edfc59de85dd46e7c00d77ed88a80b4656
James Logue <logueja@gmail.com>
2017-12-04 13:51:44 -0700

    Add tests for shooting, player hit, losing lives, player dead state

--- test/Player-test.js
+++ test/Player-test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai')
 const Player = require('../lib/Player.js')
 const Player1 = require('../lib/Player1.js')
 const Keyboarder = require('../lib/Keyboarder.js')
+const Arrow = require('../lib/Arrow.js')
 
 describe('Player', () => {
   let platforms
@@ -9,7 +10,7 @@ describe('Player', () => {
   let player1
 
   beforeEach(function() {
-    keyboarder = {keyState: {87: false, 65: false, 68:false}, isDown: function isDown(keyCode) {
+    keyboarder = {keyState: {87: false, 65: false, 68:false, 16:false}, isDown: function isDown(keyCode) {
     return this.keyState[keyCode] === true;
   }}
     platforms =[{x: 0, y: 150, width: 400, height: 100}, {x:0, y:0, width:150, height: 150}, {x:250, y:0, width:150, height: 150}]
@@ -26,6 +27,10 @@ describe('Player', () => {
 
   it('should not be dead at the start of game', () => {
     expect(player1.dead).to.equal(false)
+  })
+
+  it('should have a direction', () => {
+    expect(player1.direction === 'left' || player1.direction === 'right').to.equal(true)
   })
 
   it('should have a positive y velocity when subject to gravity', () => {
@@ -156,7 +161,107 @@ describe('Player', () => {
     player1.move()
     expect(player1.isColliding(player1.platforms)).to.equal(true);
     expect(player1.dx === -beforeCollisionDx).to.equal(true);
-
   })
+
+  it('should be able to shoot an arrow', () => {
+    expect(player1.arrows.length === 0).to.equal(true)
+    player1.arrow();
+    expect(player1.arrows.length > 0).to.equal(true)
+  })
+
+  it('should have less arrows in its quiver after it shoots', () => {
+    expect(player1.arrows.length === 0).to.equal(true)
+    let fullQuiver = player1.quiver;
+    player1.arrow();
+    expect(player1.arrows.length > 0).to.equal(true)
+    expect(player1.quiver < fullQuiver).to.equal(true)
+  })
+
+  it('should not be able to shoot when the its quiver is empty', () => {
+    expect(player1.quiver > 0).to.equal(true);
+    expect(player1.canShoot).to.equal(true);
+    player1.arrow();
+    player1.arrow();
+    player1.arrow();
+    expect(player1.quiver === 0).to.equal(true);
+    expect(player1.canShoot).to.equal(true);
+    expect(player1.arrows.length === 3).to.equal(true);
+    player1.arrow()
+    expect(player1.arrows.length === 3).to.equal(true);
+  })
+
+  it('should be able to reload its quiver after a delay', () => {
+    let fullQuiver = player1.quiver;
+    expect(player1.canReload).to.equal(true)
+    player1.arrow();
+    expect(player1.arrows.length > 0).to.equal(true)
+    expect(player1.quiver < fullQuiver).to.equal(true)
+    expect(player1.canReload).to.equal(false)
+
+    const checkReload = () => {
+      setTimeout(() => {
+      expect(player1.canReload).to.equal(true)
+      expect(player1.canShoot).to.equal(true)
+      }, 3000)
+    };
+
+    checkReload()
+  })
+
+  it('should be able to shoot based on keyboard input', () => {
+    expect(player1.arrows.length === 0).to.equal(true);
+    expect(player1.keyboarder.keyState['16']).to.equal(false)
+    expect(player1.canShoot).to.equal(true)
+    player1.move();
+    expect(player1.arrows.length === 0).to.equal(true);
+
+    player1.keyboarder.keyState['16'] = true;
+    player1.move();
+
+    expect(player1.shooting).to.equal(true)
+    expect(player1.arrows.length > 0).to.equal(true);
+  })
+
+  it('should be dead if it is hit', () => {
+    expect(player1.dead).to.equal(false);
+    player1.hit()
+    expect(player1.dead).to.equal(true);
+  })
+
+  it('should lose a life when it is dead', () => {
+    let beforeHitLives = player1.lives;
+    expect(player1.dead).to.equal(false);
+    player1.hit()
+    expect(player1.dead).to.equal(true);
+    expect(player1.lives < beforeHitLives)
+  })
+
+  it('should be alive again after a time if it has lives left', () => {
+    expect(player1.dead).to.equal(false);
+    player1.hit()
+    expect(player1.dead).to.equal(true);
+    expect(player1.lives > 0)
+
+    const checkRespawn = () => {
+      setTimeout(() => {
+      expect(player1.dead).to.equal(false)
+      player1.hit()
+      }, 2000)
+    };
+    checkRespawn()
+   
+    const checkAfterThreeHits = () => {
+      setTimeout(() => {
+        player1.hit()
+        expect(player1.lives === 0).to.equal(true)
+        player1.hit()
+        expect(player1.lives === 0).to.equal(true)
+        expect(player1.dead).to.equal(true)
+      }, 2000)
+    };
+    checkAfterThreeHits()
+  })
+
+
 
 });
